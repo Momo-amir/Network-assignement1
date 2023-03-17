7b1e051e24138ddb5596443f7feb601c96a41016
Tec <Tec@SKAB1-PC10>
2023-03-17 10:47:51 +0100

    Add project files.

---
+++ BlazorApp1/Data/Meme.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Data
+{
+    public class Meme
+    {
+        public int Id { get; set; }
+        public string MemeName { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ BlazorApp1/Data/Sql.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Transactions;
+
+namespace BlazorApp1.Data
+
+{
+    public class Sql
+    {
+
+
+
+        public static string connectionString = "Data Source=192.168.1.3;Initial Catalog=MemeDB;User ID=sa;Password=Passw0rd;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static List<Meme> Read()
+        {
+            List<Meme> list = new List<Meme>();
+            SqlConnection con = new(connectionString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM MemeTable", con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Meme meme = new Meme() { Id = dr.GetInt32(0), MemeName = dr.GetString(1), Url = dr.GetString(2) };
+                list.Add(meme);
+            }
+            con.Close();
+
+            return list;
+        }
+
+        public static void Create(Meme meme)
+        {
+            using (SqlConnection conn = new(connectionString))
+            {
+                SqlCommand cmd = new("INSERT INTO MemeTable (MemeName, Url) VALUES (@memeName, @memeUrl)", conn);
+                cmd.Parameters.Add("@memeName", SqlDbType.NVarChar).Value = meme.MemeName;
+                cmd.Parameters.Add("@memeUrl", SqlDbType.NVarChar).Value = meme.Url;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        public static void Delete(int id)
+        {
+            using (SqlConnection conn = new(connectionString))
+            {
+                SqlCommand cmd = new("DELETE FROM MemeTable WHERE Id = @id", conn);
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+    }
+}
